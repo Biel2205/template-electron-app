ec94d70cd5a46ced86b0614988d431d5e0f94c8e
Allan <teixeiraallan312@gmail.com>
2025-05-12 22:05:26 -0300

    mexendo-no-aplicativo

---
+++ main.js
@@ -0,0 +1,22 @@
+// Importa módulos app e BrowserWindow do electron
+const { app, BrowserWindow } = require('electron');
+
+// Função para criar a janela principal
+function createwindow(){
+
+// Criar uma nova instancia do BrowserWindow
+    const window = new BrowserWindow({
+
+        width: 800, // Largura da janela
+        height: 600, // Altura da janela
+
+    })
+
+    // Carregar o arquivo HTML na janela 
+    window.loadFile('index.html')
+}
+
+// Evento que é acionado quando o Electron está pronto
+app.whenReady().then( () => {
+    createwindow(); // Chama função de criar a janela
+} )
